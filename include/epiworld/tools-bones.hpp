6d25a66e2e0e2343c2a80a362eb89439b5c7bd10
George G. Vega Yon <g.vegayon@gmail.com>
2022-05-24 10:19:52 -0600

    Adding missing hpp

---
+++ include/epiworld/tools-bones.hpp
@@ -0,0 +1,154 @@
+#ifndef EPIWORLD_TOOLS_BONES_HPP
+#define EPIWORLD_TOOLS_BONES_HPP
+
+template<typename TSeq>
+class Tool;
+
+template<typename TSeq>
+class Person;
+
+#define TOOLPTR std::shared_ptr< Tool<TSeq> >
+
+/**
+ * @brief Set of tools (useful for building iterators)
+ * 
+ * @tparam TSeq 
+ */
+template<typename TSeq>
+class Tools {
+    friend class Tool<TSeq>;
+    friend class Person<TSeq>;
+private:
+    std::vector< TOOLPTR > * dat;
+    const epiworld_fast_uint * n_tools;
+
+public:
+
+    Tools() = delete;
+    Tools(Person<TSeq> & p) : dat(&p.tools), n_tools(&p.n_tools) {};
+
+    typename std::vector< TOOLPTR >::iterator begin();
+    typename std::vector< TOOLPTR >::iterator end();
+
+    TOOLPTR & operator()(size_t i);
+    TOOLPTR & operator[](size_t i);
+
+    size_t size() const noexcept;
+
+};
+
+template<typename TSeq>
+inline typename std::vector< TOOLPTR >::iterator Tools<TSeq>::begin()
+{
+
+    if (*n_tools == 0u)
+        return dat->end();
+    
+    return dat->begin();
+}
+
+template<typename TSeq>
+inline typename std::vector< TOOLPTR >::iterator Tools<TSeq>::end()
+{
+     
+    return begin() + *n_tools;
+}
+
+template<typename TSeq>
+inline TOOLPTR & Tools<TSeq>::operator()(size_t i)
+{
+
+    if (i >= *n_tools)
+        throw std::range_error("Tool index out of range.");
+
+    return dat->operator[](i);
+
+}
+
+template<typename TSeq>
+inline TOOLPTR & Tools<TSeq>::operator[](size_t i)
+{
+
+    return dat->operator[](i);
+
+}
+
+template<typename TSeq>
+inline size_t Tools<TSeq>::size() const noexcept 
+{
+    return *n_tools;
+}
+
+/**
+ * @brief Set of Tools (const) (useful for iterators)
+ * 
+ * @tparam TSeq 
+ */
+template<typename TSeq>
+class Tools_const {
+    friend class Tool<TSeq>;
+    friend class Person<TSeq>;
+private:
+    const std::vector< TOOLPTR > * dat;
+    const epiworld_fast_uint * n_tools;
+
+public:
+
+    Tools_const() = delete;
+    Tools_const(const Person<TSeq> & p) : dat(&p.tools), n_tools(&p.n_tools) {};
+
+    typename std::vector< TOOLPTR >::const_iterator begin();
+    typename std::vector< TOOLPTR >::const_iterator end();
+
+    const TOOLPTR & operator()(size_t i);
+    const TOOLPTR & operator[](size_t i);
+
+    size_t size() const noexcept;
+
+};
+
+template<typename TSeq>
+inline typename std::vector< TOOLPTR >::const_iterator Tools_const<TSeq>::begin() {
+
+    if (*n_tools == 0u)
+        return dat->end();
+    
+    return dat->begin();
+}
+
+template<typename TSeq>
+inline typename std::vector< TOOLPTR >::const_iterator Tools_const<TSeq>::end() {
+     
+    return begin() + *n_tools;
+}
+
+template<typename TSeq>
+inline const TOOLPTR & Tools_const<TSeq>::operator()(size_t i)
+{
+
+    if (i >= *n_tools)
+        throw std::range_error("Tool index out of range.");
+
+    return dat->operator[](i);
+
+}
+
+template<typename TSeq>
+inline const TOOLPTR & Tools_const<TSeq>::operator[](size_t i)
+{
+
+    return dat->operator[](i);
+
+}
+
+template<typename TSeq>
+inline size_t Tools_const<TSeq>::size() const noexcept 
+{
+    return *n_tools;
+}
+
+#undef TOOLPTR
+
+
+
+#endif
---
+++ include/epiworld/viruses-bones.hpp
@@ -0,0 +1,154 @@
+#ifndef EPIWORLD_VIRUSES_BONES_HPP
+#define EPIWORLD_VIRUSES_BONES_HPP
+
+template<typename TSeq>
+class Virus;
+
+template<typename TSeq>
+class Person;
+
+#define VIRUSPTR std::shared_ptr< Virus<TSeq> >
+
+/**
+ * @brief Set of viruses (useful for building iterators)
+ * 
+ * @tparam TSeq 
+ */
+template<typename TSeq>
+class Viruses {
+    friend class Virus<TSeq>;
+    friend class Person<TSeq>;
+private:
+    std::vector< VIRUSPTR > * dat;
+    const epiworld_fast_uint * n_viruses;
+
+public:
+
+    Viruses() = delete;
+    Viruses(Person<TSeq> & p) : dat(&p.viruses), n_viruses(&p.n_viruses) {};
+
+    typename std::vector< VIRUSPTR >::iterator begin();
+    typename std::vector< VIRUSPTR >::iterator end();
+
+    VIRUSPTR & operator()(size_t i);
+    VIRUSPTR & operator[](size_t i);
+
+    size_t size() const noexcept;
+
+};
+
+template<typename TSeq>
+inline typename std::vector< VIRUSPTR >::iterator Viruses<TSeq>::begin()
+{
+
+    if (*n_viruses == 0u)
+        return dat->end();
+    
+    return dat->begin();
+}
+
+template<typename TSeq>
+inline typename std::vector< VIRUSPTR >::iterator Viruses<TSeq>::end()
+{
+     
+    return begin() + *n_viruses;
+}
+
+template<typename TSeq>
+inline VIRUSPTR & Viruses<TSeq>::operator()(size_t i)
+{
+
+    if (i >= *n_viruses)
+        throw std::range_error("Virus index out of range.");
+
+    return dat->operator[](i);
+
+}
+
+template<typename TSeq>
+inline VIRUSPTR & Viruses<TSeq>::operator[](size_t i)
+{
+
+    return dat->operator[](i);
+
+}
+
+template<typename TSeq>
+inline size_t Viruses<TSeq>::size() const noexcept 
+{
+    return *n_viruses;
+}
+
+/**
+ * @brief Set of Viruses (const) (useful for iterators)
+ * 
+ * @tparam TSeq 
+ */
+template<typename TSeq>
+class Viruses_const {
+    friend class Virus<TSeq>;
+    friend class Person<TSeq>;
+private:
+    const std::vector< VIRUSPTR > * dat;
+    const epiworld_fast_uint * n_viruses;
+
+public:
+
+    Viruses_const() = delete;
+    Viruses_const(const Person<TSeq> & p) : dat(&p.viruses), n_viruses(&p.n_viruses) {};
+
+    typename std::vector< VIRUSPTR >::const_iterator begin();
+    typename std::vector< VIRUSPTR >::const_iterator end();
+
+    const VIRUSPTR & operator()(size_t i);
+    const VIRUSPTR & operator[](size_t i);
+
+    size_t size() const noexcept;
+
+};
+
+template<typename TSeq>
+inline typename std::vector< VIRUSPTR >::const_iterator Viruses_const<TSeq>::begin() {
+
+    if (*n_viruses == 0u)
+        return dat->end();
+    
+    return dat->begin();
+}
+
+template<typename TSeq>
+inline typename std::vector< VIRUSPTR >::const_iterator Viruses_const<TSeq>::end() {
+     
+    return begin() + *n_viruses;
+}
+
+template<typename TSeq>
+inline const VIRUSPTR & Viruses_const<TSeq>::operator()(size_t i)
+{
+
+    if (i >= *n_viruses)
+        throw std::range_error("Virus index out of range.");
+
+    return dat->operator[](i);
+
+}
+
+template<typename TSeq>
+inline const VIRUSPTR & Viruses_const<TSeq>::operator[](size_t i)
+{
+
+    return dat->operator[](i);
+
+}
+
+template<typename TSeq>
+inline size_t Viruses_const<TSeq>::size() const noexcept 
+{
+    return *n_viruses;
+}
+
+#undef VIRUSPTR
+
+
+
+#endif
