bf95df398f15a7aef17effdbe90da1a0ab679b2c
George G. Vega Yon <g.vegayon@gmail.com>
2022-09-01 09:14:10 -0600

    Removing fat

--- rpackage/R/epiworld-package.R
+++
@@ -1,9 +0,0 @@
-## usethis namespace: start
-#' @importFrom Rcpp sourceCpp
-## usethis namespace: end
-NULL
-
-## usethis namespace: start
-#' @useDynLib epiworld, .registration = TRUE
-## usethis namespace: end
-NULL
