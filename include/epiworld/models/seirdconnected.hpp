85acb6a2ff58175bd67b78f99569d23b492f2950
Derek Meyer <105825983+derekmeyer37@users.noreply.github.com>
2023-08-04 09:27:03 -0600

    Update seirdconnected.hpp

--- include/epiworld/models/seirdconnected.hpp
+++ include/epiworld/models/seirdconnected.hpp
@@ -286,7 +286,6 @@ inline ModelSEIRDCONN<TSeq>::ModelSEIRDCONN(
     epiworld::Virus<TSeq> virus(vname);
     virus.set_state(
         ModelSEIRDCONN<TSeq>::EXPOSED,
-        ModelSEIRDCONN<TSeq>::INFECTED,
         ModelSEIRDCONN<TSeq>::REMOVED,
         ModelSEIRDCONN<TSeq>::DECEASED
         );
