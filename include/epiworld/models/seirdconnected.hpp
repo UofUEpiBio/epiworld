bb8dc64d0ef84c96681f7fb0d5638801dc76b5d2
Derek Meyer <derekmeyer37@gmail.com>
2023-08-08 11:12:17 -0600

    updating seirdconn

--- include/epiworld/models/seirdconnected.hpp
+++ include/epiworld/models/seirdconnected.hpp
@@ -222,41 +222,55 @@ inline ModelSEIRDCONN<TSeq>::ModelSEIRDCONN(
             {
 
 
-                // Odd: Die, Even: Recover
-                epiworld_fast_uint n_events = 0u;
-                for (const auto & v : p->get_viruses())
-                {
-
-                    // Recover
-                    m->array_double_tmp[n_events++] = 
-                        1.0 - (1.0 - v->get_prob_recovery(m)) * (1.0 - p->get_recovery_enhancer(v, m)); 
-
-                }
-
-                #ifdef EPI_DEBUG
-                if (n_events == 0u)
-                {
-                    printf_epiworld(
-                        "[epi-debug] agent %i has 0 possible events!!\n",
-                        static_cast<int>(p->get_id())
-                        );
-                    throw std::logic_error("Zero events in exposed.");
-                }
-                #else
-                if (n_events == 0u)
-                    return;
-                #endif
+              // Odd: Die, Even: Recover
+              epiworld_fast_uint n_events = 0u;
+              for (const auto & v : p->get_viruses())
+              {
                 
-
-                // Running the roulette
-                int which = roulette(n_events, m);
-
-                if (which < 0)
-                    return;
-
-                // Which roulette happen?
+                // Die
+                m->array_double_tmp[n_events++] = 
+                  v->get_prob_death(m) * (1.0 - p->get_death_reduction(v, m)); 
+                
+                // Recover
+                m->array_double_tmp[n_events++] = 
+                  1.0 - (1.0 - v->get_prob_recovery(m)) * (1.0 - p->get_recovery_enhancer(v, m)); 
+                
+              }
+              
+              #ifdef EPI_DEBUG
+              if (n_events == 0u)
+              {
+                printf_epiworld(
+                  "[epi-debug] agent %i has 0 possible events!!\n",
+                  static_cast<int>(p->get_id())
+                );
+                throw std::logic_error("Zero events in exposed.");
+              }
+              #else
+              if (n_events == 0u)
+                return;
+              #endif
+              
+              
+              // Running the roulette
+              int which = roulette(n_events, m);
+              
+              if (which < 0)
+                return;
+              
+              // Which roulette happen?
+              if ((which % 2) == 0) // If odd
+              {
+                
+                size_t which_v = std::ceil(which / 2);
+                p->rm_agent_by_virus(which_v, m);
+                
+              } else {
+                
                 size_t which_v = std::floor(which / 2);
                 p->rm_virus(which_v, m);
+                
+              }
 
                 return ;
 
