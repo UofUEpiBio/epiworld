53e7fe5f86a865c2b0900b99aa12c94fd34e0561
George Vega Yon <g.vegayon@gmail.com>
2024-07-15 23:38:25 +0000

    Working on generation interval

--- examples/13-genint/main.cpp
+++ examples/13-genint/main.cpp
@@ -6,15 +6,124 @@
 
 using namespace epiworld;
 
+// Implementing the factorial function
+inline double log_factorial(int n)
+{
+    if (n == 0)
+        return 0.0;
+    return std::log(static_cast<double>(n)) + log_factorial(n-1);
+}
+
+inline double dpois(int k, double lambda, int max_n = 100, bool as_log = false)
+{
+
+    if (max_n < k)
+        throw std::runtime_error("max_n must be greater than k");
+
+    double res = k * std::log(lambda) - lambda - log_factorial(
+        std::min(k, max_n)
+        );
+    
+    return as_log ? res : std::exp(res);
+}
+
+/**
+ * @brief Compute the probability of the generation interval
+ * 
+ * @details
+ * If `p_0_approx` is negative, it will be computed using the Poisson
+ * distribution.
+ * 
+ * @param g Generation interval
+ * @param S Population size
+ * @param p_c Probability of contact
+ * @param p_i Probability of infection
+ * @param p_r Probability of recovery
+ * @param p_0_approx Approximation of the probability of not being infected
+ * @param max_n Maximum number of contacts
+ * 
+ * @return The probability of the generation interval
+ * 
+ */
+double dgenint(
+    int g,
+    double S,
+    double p_c,
+    double p_i,
+    double p_r,
+    double & p_0_approx,
+    int max_n = 500
+    ) {
+
+    if (p_0_approx < 0.0)
+    {
+
+        p_0_approx = 0.0;
+        for (int i = 0; i < max_n; ++i)
+        {
+
+            p_0_approx +=
+                dpois(i, S * p_c, max_n, false) *
+                std::pow(1.0 - p_i, static_cast<double>(i)) ;
+
+        }
+    }
+
+    double g_dbl = static_cast<double>(g);
+
+    return
+        std::pow(1 - p_r, g_dbl) *
+        std::pow(p_0_approx, g_dbl - 1.0) * 
+        (1.0 - p_0_approx);
+
+}
+
+// Mean of the generation interval
+/**
+ * @brief Compute the mean of the generation interval
+ * @param S Population size
+ * @param p_c Probability of contact
+ * @param p_i Probability of infection
+ * @param p_r Probability of recovery
+ * @param max_n Maximum number of contacts
+ * 
+ * @return The mean of the generation interval
+ */
+double gen_int_mean(
+    double S,
+    double p_c,
+    double p_i,
+    double p_r,
+    int max_n = 200
+    ) {
+
+    double mean = 0.0;
+    double p_0_approx = -1.0;
+    for (int i = 0; i < max_n; ++i)
+    {
+        mean += 
+            static_cast<double>(i) *
+            dgenint(
+                static_cast<double>(i), S, p_c, p_i, p_r, p_0_approx, max_n
+                );
+
+        if (i == 0)
+            std::cout << "p_0_approx: " << p_0_approx << std::endl;
+    }
+
+    return mean;
+
+}
+
 int main()
 {
 
     // Model parameters
     double r   = 5.0; // Contact rate
-    double popsize = 5e5;
+    double popsize = 1e4;
     double p_contact = r/popsize; // Probability of contact
-    double p_i = .05; // Probability of infection
-    double p_r = 1.0/7.0; // Probability of recovery
+    double p_i = .1; // Probability of infection
+    double p_r = 1.0/15.0; // Probability of recovery
 
     size_t nsims = 1e6; // Number of simulations
     size_t nsteps = 100; // Number of days
@@ -95,7 +204,7 @@ int main()
         50.0/popsize,
         r,
         p_i,
-        0.000009,
+        0.00000000009,
         p_r
     );
 
@@ -124,6 +233,10 @@ int main()
     std::cout << "Mean  (naive)    : " << mean << std::endl;
     std::cout << "Mean2 (from sim) : " << mean2 << std::endl;
     std::cout << "Mean  (SEIRCONN) : " << mean_seirconn << std::endl;
+
+    double theory_mean = gen_int_mean(popsize, p_contact, p_i, p_r);
+    std::cout << "Mean  (theory) +1: " << theory_mean + 1.0 << std::endl;
+    
 
     return 0;
 
