ae7661151a3ac5084fa3c7d795bc80642d27f8dd
George G. Vega Yon <g.vegayon@gmail.com>
2025-03-14 23:21:41 +0000

    Bump version

--- epiworld.hpp
+++ epiworld.hpp
@@ -20,8 +20,8 @@
 
 /* Versioning */
 #define EPIWORLD_VERSION_MAJOR 0
-#define EPIWORLD_VERSION_MINOR 7
-#define EPIWORLD_VERSION_PATCH 2
+#define EPIWORLD_VERSION_MINOR 8
+#define EPIWORLD_VERSION_PATCH 0
 
 static const int epiworld_version_major = EPIWORLD_VERSION_MAJOR;
 static const int epiworld_version_minor = EPIWORLD_VERSION_MINOR;
