7d6b1746b370343cc53c30511434dd8d3c490707
Milo Banks <milobanks@rowlandhall.org>
2025-05-13 23:26:06 -0600

    [test] fix method referenced names

--- tests/14b-measles.cpp
+++ tests/14b-measles.cpp
@@ -43,10 +43,10 @@ EPIWORLD_TEST_CASE("Measles model (quarantine)", "[ModelMeaslesQuarantineOn]") {
     auto saver = [&transitions, &R0s, n_seeds](size_t n, Model<>* m) -> void{
 
         // Saving the transition probabilities
-        transitions[n] = m->get_db().transition_probability(false, false);
+        transitions[n] = m->get_db().get_transition_probability(false, false);
 
         // Recording the R0 from the index case
-        auto rts = m->get_db().reproductive_number();      
+        auto rts = m->get_db().get_reproductive_number();      
         for (int i = 0; i < n_seeds; ++i)
             R0s[n_seeds * n + i] = static_cast<epiworld_double>(rts[{0, i, 0}]);
 
