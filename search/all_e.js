7cc1c1d91b0cfc57ef10398ad43f0bcc440f952f
milobanks <milobanks@users.noreply.github.com>
2025-05-14 05:48:47 +0000

    deploy: c2ca635200a9cad787b5ee194859397289d02e29

--- search/all_e.js
+++ search/all_e.js
@@ -6,7 +6,7 @@ var searchData=
   ['set_5fagents_5fdata_95',['set_agents_data',['../class_model.html#a14758e0476fd4a05643ab41190530513',1,'Model::set_agents_data()'],['../classepiworld_1_1_model.html#a14758e0476fd4a05643ab41190530513',1,'epiworld::Model::set_agents_data(double *data_, size_t ncols_)']]],
   ['set_5fname_96',['set_name',['../classepiworld_1_1_model.html#a453485b4cfe2a51addf33003f8d70c36',1,'epiworld::Model::set_name()'],['../class_model.html#a453485b4cfe2a51addf33003f8d70c36',1,'Model::set_name()']]],
   ['set_5fuser_5fdata_97',['set_user_data',['../classepiworld_1_1_model.html#adc8d49825ecf57ac045a4c93e4d63924',1,'epiworld::Model::set_user_data()'],['../class_model.html#adc8d49825ecf57ac045a4c93e4d63924',1,'Model::set_user_data()']]],
-  ['state_5ffun_98',['state_fun',['../classepiworld_1_1_model.html#abdb996c209c273b52baa1b27aa1f2b6d',1,'epiworld::Model::state_fun()'],['../class_model.html#a368290d024872bf73ecf46c9885ba16b',1,'Model::state_fun()']]],
-  ['states_5flabels_99',['states_labels',['../classepiworld_1_1_model.html#afec625f0510bfd970aed3eff513774aa',1,'epiworld::Model::states_labels()'],['../class_model.html#a1b047545d828ca048053c20e9d0695c4',1,'Model::states_labels()']]],
+  ['state_5ffun_98',['state_fun',['../classepiworld_1_1_model.html#a750bf28646dc76c4e394d8d6cc217f3b',1,'epiworld::Model::state_fun()'],['../class_model.html#a2175c4bfbc6f8b8e0146f08162b03dbb',1,'Model::state_fun()']]],
+  ['states_5flabels_99',['states_labels',['../classepiworld_1_1_model.html#aa49297ff1640a8f6e7c8ff6b8bce6b65',1,'epiworld::Model::states_labels()'],['../class_model.html#a658a1f7bc9a3fa727788353e44a309f0',1,'Model::states_labels()']]],
   ['swap_5fneighbors_100',['swap_neighbors',['../classepiworld_1_1_agent.html#a938c535d35ff860eb59505c36a919df0',1,'epiworld::Agent::swap_neighbors()'],['../class_agent.html#a938c535d35ff860eb59505c36a919df0',1,'Agent::swap_neighbors()']]]
 ];
