e1b66477b2cfbc9bfaa5e4ac2f653f79a783fedf
milobanks <milobanks@users.noreply.github.com>
2025-05-28 14:29:02 +0000

    deploy: 20cd51eb2a0007daf61acf1ec571a069c03ce461

--- search/functions_2.js
+++ search/functions_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['distribute_169',['distribute',['../classepiworld_1_1_virus.html#a918adfceb40159f1bc4f666258544784',1,'epiworld::Virus::distribute()'],['../class_virus.html#a918adfceb40159f1bc4f666258544784',1,'Virus::distribute()']]],
-  ['draw_170',['draw',['../classepiworld_1_1_model.html#a6a6f3fd87283f3cd3a7216ef67ff4e4a',1,'epiworld::Model::draw()'],['../class_model.html#a9a3f0255dfa3c7b7f673146ac5f0ffda',1,'Model::draw()']]]
+  ['draw_170',['draw',['../classepiworld_1_1_model.html#a6a6f3fd87283f3cd3a7216ef67ff4e4a',1,'epiworld::Model::draw()'],['../class_model.html#a6a6f3fd87283f3cd3a7216ef67ff4e4a',1,'Model::draw()']]]
 ];
