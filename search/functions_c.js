94690fb0412d56abdb3da96245212655fd678df9
milobanks <milobanks@users.noreply.github.com>
2025-06-04 05:16:00 +0000

    deploy: bfefdb14d2570e2ab96fef5e362d935b8da8d941

--- search/functions_c.js
+++ search/functions_c.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['today_223',['today',['../classepiworld_1_1_model.html#aa3ce8d5f025e239f8090ab9645f3e917',1,'epiworld::Model::today()'],['../class_model.html#aa3ce8d5f025e239f8090ab9645f3e917',1,'Model::today()']]]
+  ['today_221',['today',['../class_model.html#aa3ce8d5f025e239f8090ab9645f3e917',1,'Model']]]
 ];
