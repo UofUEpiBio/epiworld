79922d52c20a7d27c53faf9c3bd1f73b2d4bcc51
George G. Vega Yon <g.vegayon@gmail.com>
2025-06-10 13:40:00 -0600

    copilot sonnet 4 fixed duplication in single header

--- epiworld-hpp.R
+++ epiworld-hpp.R
@@ -1,7 +1,8 @@
 line78 <- paste(rep("/", 78), collapse = "")
 
 N_CALLS   <- 0L
-MAX_CALLS <- 10L
+MAX_CALLS <- 50L
+INCLUDED_FILES <- character(0)
 
 unfolder <- function(txt, rel = "include/epiworld/") {
 
@@ -38,12 +39,43 @@ unfolder <- function(txt, rel = "include/epiworld/") {
         loc <- heads[h]
 
         fn <- trimws(paste0(rel, fns[h]))
+        
+        # Normalize the file path to detect true duplicates
+        # This resolves relative paths like "../" to the actual file location
+        tryCatch({
+            fn_normalized <- normalizePath(fn, mustWork = TRUE)
+        }, error = function(e) {
+            fn_normalized <<- fn
+        })
+        
+        # Check if this file has already been included (use normalized path)
+        if (fn_normalized %in% INCLUDED_FILES) {
+            # Remove the include line
+            new_src <- c(
+                new_src[1:(loc - 1)],
+                new_src[(loc + 1):length(new_src)]
+            )
+            next
+        }
+        
+        # Mark this file as included (use normalized path)
+        INCLUDED_FILES <<- c(INCLUDED_FILES, fn_normalized)
+        
         tmp_lines <- readLines(fn, warn = FALSE)
 
         # Extracting relative path
         sub_rel <- gsub("[^/]+$", "", fns[h])
 
-        tmp_lines <- unfolder(tmp_lines, rel = paste0(rel, "/", sub_rel))
+        # Fix the relative path construction
+        if (sub_rel == "" || sub_rel == "./") {
+            new_rel <- rel
+        } else {
+            # Remove trailing slash from rel if present, then add sub_rel
+            rel_clean <- gsub("/$", "", rel)
+            new_rel <- paste0(rel_clean, "/", sub_rel)
+        }
+
+        tmp_lines <- unfolder(tmp_lines, rel = new_rel)
 
         # Getting the filename
         new_src <- c(
@@ -61,6 +93,9 @@ unfolder <- function(txt, rel = "include/epiworld/") {
 }
 
 rel <- "include/epiworld/"
+
+# Reset the included files list for each run
+INCLUDED_FILES <- character(0)
 
 # Barry core
 src <- readLines(paste0(rel, "epiworld.hpp"), warn = FALSE)
