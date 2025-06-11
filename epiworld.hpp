a2ee81c9a82198c1c8810272cb8221414afa586f
George G. Vega Yon <g.vegayon@gmail.com>
2025-06-11 12:03:31 -0600

    Re-running the header

--- epiworld.hpp
+++ epiworld.hpp
@@ -12114,6 +12114,9 @@ inline void Queue<TSeq>::operator+=(Agent<TSeq> * p)
     if (++active[p->id] == 1)
         n_in_queue++;
 
+    if (p->get_n_neighbors() == 0u)
+        return; // No neighbors, no need to add them
+
     for (auto n : (*p->neighbors))
     {
 
@@ -12130,6 +12133,9 @@ inline void Queue<TSeq>::operator-=(Agent<TSeq> * p)
 
     if (--active[p->id] == 0)
         n_in_queue--;
+
+    if (p->get_n_neighbors() == 0u)
+        return; // No neighbors, no need to add them
 
     for (auto n : (*p->neighbors))
     {
@@ -21387,7 +21393,9 @@ inline Agent<TSeq>::Agent(const Agent<TSeq> & p) :
     n_neighbors(p.n_neighbors),
     entities(p.entities),
     entities_locations(p.entities_locations),
-    n_entities(p.n_entities)
+    n_entities(p.n_entities),
+    neighbors(nullptr),
+    neighbors_locations(nullptr)
 {
 
     if (n_neighbors > 0u)
@@ -21440,6 +21448,11 @@ inline Agent<TSeq> & Agent<TSeq>::operator=(
     {
         neighbors = new std::vector< size_t >(*other_agent.neighbors);
         neighbors_locations = new std::vector< size_t >(*other_agent.neighbors_locations);
+    }
+    else 
+    {
+        neighbors = nullptr;
+        neighbors_locations = nullptr;
     }
     
     entities = other_agent.entities;
