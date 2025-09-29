e6fccbafe543191befa05e926d086c234d8bd339
Olivia Banks <elegant.lion1334@fastmail.com>
2025-09-29 12:00:35 -0600

    [test/issue] fix #91

--- tests/01-sample.cpp
+++ tests/01-sample.cpp
@@ -13,21 +13,21 @@ int main()
 
     // Declaring the three statuses in the model
     model.add_state("Susceptible", epiworld::default_update_susceptible<>);
-    model.add_state("Exposed", epiworld::default_update_exposed<>);
-    model.add_state("Recovered");
-    model.add_state("Removed");
+    auto exposed_state = model.add_state("Exposed", epiworld::default_update_exposed<>);
+    auto recovered_state = model.add_state("Recovered");
+    auto removed_state = model.add_state("Removed");
 
     // Adding the tool and virus
     Virus<> virus("covid 19", 50, false);
     virus.set_post_immunity(1.0);
-    virus.set_state(1,2,3);
+    virus.set_state(exposed_state, recovered_state, removed_state);
     virus.set_prob_death(.01);
     model.add_virus(virus);
-    
+
     epiworld::Tool<> tool("vaccine", .5, true);
     model.add_tool(tool);
 
-    // Generating a random pop 
+    // Generating a random pop
     model.agents_smallworld(10000);
 
     // Running the model
@@ -44,5 +44,5 @@ int main()
         a->print();
 
 
-  
+
 }
